d5a9340befc594d84a27ae1e7699eaa232e207af
Christine Wong <jumiryoo@gmail.com>
2023-09-16 09:43:42 -0700

    building todos app

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bsh
+
+# exit when any command fails
+set -o errexit
+
+## Install dependencies via pip
+pip3 install -r dependencies.txt
+
+## Run migration just in case
+python3 manage.py migrate
+
