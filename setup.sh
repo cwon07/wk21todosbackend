d4058749076418348d6f3cc32d7b113bdbd83db0
Christine Wong <jumiryoo@gmail.com>
2023-09-16 10:05:33 -0700

    setup.sh change

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env bsh
+#!/usr/bin/env bash
 
 # exit when any command fails
 set -o errexit
 
 ## Install dependencies via pip
-pip3 install -r dependencies.txt
+pip install -r dependencies.txt
 
 ## Run migration just in case
-python3 manage.py migrate
-
+python manage.py migrate
